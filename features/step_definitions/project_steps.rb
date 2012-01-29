36d2afeea023d138d3f4e8534782467a35ae916e
Josh Lin <joshlin@uw.edu>
2012-01-29 15:46:15 -0800

    Implemented edit action for the tickets controller

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,3 +1,3 @@
-Given /^there is a project called "([^\"]*)"$/ do |name|
-	Factory(:project, :name => name)
+Given /^there is a project called "([^\"]*)"$/ do |name|	
+	@project = Factory(:project, :name => name)
 end
